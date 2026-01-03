36c7869a8ff3f2460150bd9830154f02ea37ed83
Torben <torben.reinhart@gmail.com>
2026-01-03 19:03:48 +0100

    Lektion 18 Uebung4

--- src/main/java/de/thws/Lektion18/Uebung4/TcpClientKomprimiert.java
+++ src/main/java/de/thws/Lektion18/Uebung4/TcpClientKomprimiert.java
@@ -3,16 +3,18 @@ package de.thws.Lektion18.Uebung4;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.zip.GZIPOutputStream;
 
 public class TcpClientKomprimiert {
 
     public static void main(String[] args) throws IOException {
         byte[] bytesToTransfer = "Hallo Welt\n".getBytes();
         try(Socket connectionToHost = new Socket("localhost", 5000);
-            OutputStream os = connectionToHost.getOutputStream();)
+            OutputStream os = connectionToHost.getOutputStream();
+            GZIPOutputStream gzos = new GZIPOutputStream(os))
         {
-            os.write(bytesToTransfer);
-        }catch (Exception e){
+            gzos.write(bytesToTransfer);
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
--- src/main/java/de/thws/Lektion18/Uebung4/TcpServerKomprimiert.java
+++ src/main/java/de/thws/Lektion18/Uebung4/TcpServerKomprimiert.java
@@ -1,27 +1,27 @@
 package de.thws.Lektion18.Uebung4;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.zip.GZIPInputStream;
 
 public class TcpServerKomprimiert {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final int PORT = 5000;
-        try(ServerSocket ss = new ServerSocket(PORT);
-            Socket connection = ss.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream())))
-            {
-                String clientInput;
-                while((clientInput = br.readLine()) != null){
-                    System.out.println(clientInput);
-                }
-            }catch (Exception e){
-                e.printStackTrace();
+        try (ServerSocket ss = new ServerSocket(PORT);
+             Socket connection = ss.accept();
+             InputStream is = connection.getInputStream();
+             GZIPInputStream gis = new GZIPInputStream(is);
+             InputStreamReader isr = new InputStreamReader(gis);
+             BufferedReader br = new BufferedReader(isr))
+        {
+            String clientInput;
+            while ((clientInput = br.readLine()) != null) {
+                System.out.println(clientInput);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
